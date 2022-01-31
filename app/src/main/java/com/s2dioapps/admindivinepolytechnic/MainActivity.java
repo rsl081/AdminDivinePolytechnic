d8e76653447851c425367fe521a6fa441db667cc
rsl081 <53944109+rsl081@users.noreply.github.com>
2022-01-31 12:39:55 +0800

    logout-rsl

--- app/src/main/java/com/s2dioapps/admindivinepolytechnic/MainActivity.java
+++ app/src/main/java/com/s2dioapps/admindivinepolytechnic/MainActivity.java
@@ -1,16 +1,22 @@
 package com.s2dioapps.admindivinepolytechnic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.s2dioapps.admindivinepolytechnic.databinding.ActivityMainBinding;
+import com.s2dioapps.admindivinepolytechnic.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +38,27 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.action_myProfile)
+        {
+            final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+            firebaseAuth.signOut();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
